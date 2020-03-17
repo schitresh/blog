fbd5e61a4e8a528801da0300e63b8b8f285fbdab
Chitresh <chitresh.hsharma@gmail.com>
2020-03-17 17:28:18 +0530

    added deleted file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_074953) do
+ActiveRecord::Schema.define(version: 2020_03_17_112337) do
 
   create_table "article_categories", force: :cascade do |t|
     t.integer "article_id"
